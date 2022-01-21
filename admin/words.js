ca068f2256a0534111575d76a813f276ded43002
swissglider <npm@mailschweiz.com>
2022-01-21 11:28:14 +0100

    Initial commit

---
+++ admin/words.js
@@ -0,0 +1,41 @@
+/*global systemDictionary:true */
+'use strict';
+
+systemDictionary = {
+    'thehome_template adapter settings': {
+        en: 'Adapter settings for thehome_template',
+        de: 'Adaptereinstellungen für thehome_template',
+        ru: 'Настройки адаптера для thehome_template',
+        pt: 'Configurações do adaptador para thehome_template',
+        nl: 'Adapterinstellingen voor thehome_template',
+        fr: "Paramètres d'adaptateur pour thehome_template",
+        it: "Impostazioni dell'adattatore per thehome_template",
+        es: 'Ajustes del adaptador para thehome_template',
+        pl: 'Ustawienia adaptera dla thehome_template',
+        'zh-cn': 'thehome_template的适配器设置',
+    },
+    option1: {
+        en: 'option1',
+        de: 'Option 1',
+        ru: 'Опция 1',
+        pt: 'Opção 1',
+        nl: 'Optie 1',
+        fr: 'Option 1',
+        it: 'opzione 1',
+        es: 'Opción 1',
+        pl: 'opcja 1',
+        'zh-cn': '选项1',
+    },
+    option2: {
+        en: 'option2',
+        de: 'Option 2',
+        ru: 'вариант 2',
+        pt: 'opção 2',
+        nl: 'Optie 2',
+        fr: 'Option 2',
+        it: 'opzione 2',
+        es: 'opcion 2',
+        pl: 'Opcja 2',
+        'zh-cn': '选项2',
+    },
+};
